4fea2540959dd968d2378a1409f60e496c8dbb12
Egor Nekrasow <ntesla2016@yandex.ru>
2020-09-28 10:03:15 +0500

    Initial commit

---
+++ my_programms.h
@@ -0,0 +1,14 @@
+#ifndef MY_PROGRAMMS_H_INCLUDED
+#define MY_PROGRAMMS_H_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void submenu(void);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif // MY_PROGRAMMS_H_INCLUDED
