3681c428e74feb8c1a57459f6d5f299a25ccf15e
Egor Nekrasow <ntesla2016@yandex.ru>
2020-10-12 12:58:50 +0500

    Initial commit

--- my_programms.h
+++ my_programms.h
@@ -4,14 +4,40 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+enum{
+    LEFT,
+    RIGHT,
+    BOTTOM,
+};
+typedef uint8_t lv_own_align_t;
+
 typedef struct submenu_t {
-    lv_obj_t * Lmenu;
+    lv_obj_t ** Lmenu;
     lv_obj_t * first_btns[2];// для удобства храню изначальные 2 кнопки в массиве
     lv_group_t * maingroup;// указтель на группу которой управляет инкодер
     int number_of_menu_levels;
+    lv_own_align_t align;
 } MENU;
 
-void submenu(MENU * mainmenu);
+typedef struct{
+    lv_btn_ext_t btn;
+    char ** next_menu_labels;
+    int menu_level;
+}MenuItem;
+
+
+
+void standart_menu_cb(lv_obj_t * btn,lv_event_t event);
+void remove_menu(lv_group_t * maingroup,lv_obj_t * menu);
+void rewrite_group(lv_group_t * maingroup,lv_obj_t * previous_menu);
+lv_obj_t * get_btn_in_index(lv_obj_t * menu, int index);
+void set_submenu_to_btn(MENU * mainmenu,int menu_level, int number_btn,char ** labels);
+void create_and_add_new_menu(MENU * mainmenu,lv_obj_t * triger_btn);
+void btn_set_menu_level(lv_obj_t * btn, int menu_level);
+void init_menu(MENU * mainmenu,lv_own_align_t align);
+void set_null_ptr(lv_obj_t * btn);
+void lv_menu_create(lv_obj_t * triger_btn,char ** labels,MENU * mainmenu,lv_own_align_t);
 
 #ifdef __cplusplus
 } /* extern "C" */
--- submenu_wth_struct.c
+++ submenu_wth_struct.c
@@ -1,45 +1,64 @@
 #include "lvgl/lvgl.h"
-#include "C:\Users\Егор\Desktop\работа\submenu\submenu\my_programms.h"
+
 #include <string.h>
 #include <malloc.h>
+#include "..//..//submenu/my_programms.h"
+#define _STR_ITER(labels,iter) for(;strcmp(labels[iter],"")!=0;iter++)
 
-#define _STR_ITER(labels,iter) for(iter;strcmp(labels[iter],"")!=0;iter++)
-
-
-typedef struct{
-    lv_btn_ext_t btn;
-    char ** next_menu_labels;
-    int menu_level;
-}MenuItem;
 
 void standart_menu_cb(lv_obj_t * btn,lv_event_t event){
-    int key = (int)lv_event_get_data(event);
+    int * keyptr = (int *)lv_event_get_data();
+    int key = * keyptr;
     MenuItem * Item = lv_obj_get_ext_attr(btn);
     MENU * mainmenu = lv_obj_get_user_data(btn);
     if (event == LV_EVENT_CLICKED){
-        if (Item->next_menu_labels = NULL){
-            return 0;
+        if (Item->next_menu_labels == NULL){
+            lv_obj_set_state(btn,LV_STATE_PRESSED);
         }
         else {
-            lv_menu_create(btn,Item->next_menu_labels,mainmenu);
+            lv_menu_create(btn,Item->next_menu_labels,mainmenu,mainmenu->align);
         }
     }
     else if (key == 27){
         if (Item->menu_level != 1){
-            lv_group_remove_all_objs(mainmenu->maingroup);
-            lv_obj_del(mainmenu->Lmenu[Item->menu_level]);
             rewrite_group(mainmenu->maingroup,mainmenu->Lmenu[Item->menu_level-1]);
+            remove_menu(mainmenu->maingroup,mainmenu->Lmenu[Item->menu_level]);
+            mainmenu->Lmenu = (lv_obj_t **)realloc(mainmenu->Lmenu,(Item->menu_level-1)*sizeof(lv_obj_t *));
+            mainmenu->number_of_menu_levels += -1;
+        }
+        else {
+            remove_menu(mainmenu->maingroup,mainmenu->Lmenu[Item->menu_level]);
+            mainmenu->Lmenu = (lv_obj_t **)realloc(mainmenu->Lmenu,(Item->menu_level-1)*sizeof(lv_obj_t *));
+            lv_group_add_obj(mainmenu->maingroup,mainmenu->first_btns[0]);
+            lv_group_add_obj(mainmenu->maingroup,mainmenu->first_btns[1]);
         }
     }
 }
 
-void rewrite_group(lv_group_t * maingroup){
-
-
+void remove_menu(lv_group_t * maingroup,lv_obj_t * menu){
+    lv_group_remove_all_objs(maingroup);
+    lv_obj_del(menu);
 }
 
+void rewrite_group(lv_group_t * maingroup,lv_obj_t * previous_menu){
+    lv_obj_t * btn = lv_list_get_next_btn(previous_menu,NULL);
+    while (btn != NULL){
+        lv_group_add_obj(maingroup,btn);
+        btn = lv_list_get_next_btn(previous_menu,btn);
+    }
+}
 
-void menuitem_set_submenu(lv_obj_t * btn,char ** labels){
+lv_obj_t * get_btn_in_index(lv_obj_t * menu, int index){
+    lv_obj_t * btn = lv_list_get_next_btn(menu,NULL);
+    for (int i = 0; i!=index;i++){
+        btn = lv_list_get_next_btn(menu,btn);
+    }
+    return (btn);
+}
+
+void set_submenu_to_btn(MENU * mainmenu,int menu_level, int number_btn,char ** labels){
+    lv_obj_t * menu = mainmenu->Lmenu[menu_level-1];
+    lv_obj_t * btn = get_btn_in_index(menu,number_btn-1);
     MenuItem * Item = lv_obj_get_ext_attr(btn);
     int iter = 0;
     _STR_ITER(labels,iter){
@@ -50,22 +69,15 @@ void menuitem_set_submenu(lv_obj_t * btn,char ** labels){
     Item->next_menu_labels[iter] = "";
 }
 
-lv_obj_t * create_btn_with_label(lv_obj_t * list,char * label){
-    lv_obj_t * btn;
-    btn = lv_btn_create(list,NULL);
-    lv_obj_t * btn_label = lv_label_create(btn,NULL);
-    lv_label_set_text(btn_label,label);
-    return (btn);
-}
 
 void create_and_add_new_menu(MENU * mainmenu,lv_obj_t * triger_btn){
     MenuItem * Item = lv_obj_get_ext_attr(triger_btn);
     if (Item == NULL){
-        mainmenu->Lmenu = (lv_obj_t *)realloc(mainmenu->Lmenu,sizeof(lv_obj_t));
+        mainmenu->Lmenu = (lv_obj_t **)realloc(mainmenu->Lmenu,sizeof(lv_obj_t *));
         mainmenu->Lmenu[0] = lv_list_create(lv_scr_act(),NULL);
     }
     else if (Item->menu_level >= mainmenu->number_of_menu_levels){
-        mainmenu->Lmenu = (lv_obj_t *)realloc(mainmenu->Lmenu,(Item->menu_level+1)*sizeof(lv_obj_t))
+        mainmenu->Lmenu = (lv_obj_t **)realloc(mainmenu->Lmenu,(Item->menu_level+1)*sizeof(lv_obj_t *));
         mainmenu->Lmenu[Item->menu_level] = lv_list_create(lv_scr_act(),NULL);
     }
     else if (Item->menu_level < mainmenu->number_of_menu_levels){
@@ -78,11 +90,13 @@ void btn_set_menu_level(lv_obj_t * btn, int menu_level){
     Item->menu_level = menu_level;
 }
 
-void init_menu(MENU * mainmenu){
+void init_menu(MENU * mainmenu,lv_own_align_t align){
     static int count = 0;
     if (count == 0 ){
         mainmenu->number_of_menu_levels = 0;
+        mainmenu->Lmenu = NULL;
         count++;
+        mainmenu->align = align;
     }
 }
 
@@ -91,20 +105,35 @@ void set_null_ptr(lv_obj_t * btn){
     Item->next_menu_labels = NULL;
 }
 
-void lv_menu_create(lv_obj_t * triger_btn,char * labels,MENU * mainmenu){
-    init_menu(mainmenu);
+void set_align_for_menu(lv_obj_t * trigger_btn,lv_obj_t * menu,lv_own_align_t align){
+    switch(align){
+        case LEFT:{
+        lv_obj_align(menu,trigger_btn,LV_ALIGN_OUT_LEFT_MID,0,0);
+        }
+        case RIGHT:{
+        lv_obj_align(menu,trigger_btn,LV_ALIGN_OUT_RIGHT_MID,0,0);
+        }
+        case BOTTOM:{
+        lv_obj_align(menu,trigger_btn,LV_ALIGN_OUT_BOTTOM_MID,0,0);
+        }
+    }
+}
+
+void lv_menu_create(lv_obj_t * triger_btn,char ** labels,MENU * mainmenu,lv_own_align_t align){
+    init_menu(mainmenu,align);
     int iter = 0;
     lv_group_remove_all_objs(mainmenu->maingroup);
+    lv_obj_t * newbtns;
+    mainmenu->number_of_menu_levels++;
+    set_align_for_menu(triger_btn,mainmenu->Lmenu[mainmenu->number_of_menu_levels-1],mainmenu->align);
     _STR_ITER(labels,iter){
         create_and_add_new_menu(mainmenu,triger_btn);
-        lv_obj_t * btn = create_btn_with_label(menu,labels[iter]);
-        lv_obj_allocate_ext_attr(btn, sizeof(MenuItem));
-        btn_set_menu_level(btn,menu_level);
-        lv_obj_set_user_data(mainmenu);
-        set_null_ptr(btn);
-        lv_obj_set_event_cb(btn,standart_menu_cb);
-        lv_list_add_btn(menu,btn);
-        lv_group_add_obj(mainmenu->maingroup,btn);
+        newbtns = lv_list_add_btn(mainmenu->Lmenu[mainmenu->number_of_menu_levels-1],NULL,labels[iter]);
+        lv_obj_allocate_ext_attr(newbtns, sizeof(MenuItem));
+        btn_set_menu_level(newbtns,mainmenu->number_of_menu_levels);
+        lv_obj_set_user_data(newbtns,mainmenu);
+        set_null_ptr(newbtns);
+        lv_obj_set_event_cb(newbtns,standart_menu_cb);
+        lv_group_add_obj(mainmenu->maingroup,newbtns);
     }
-    mainmenu->number_of_menu_levels++;
 }
