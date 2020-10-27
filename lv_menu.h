ba7c91e0f0adcbd85ee6c032bdd597ca423edafc
Egor Nekrasow <ntesla2016@yandex.ru>
2020-10-27 13:25:29 +0500

    Initial commit

--- lv_menu.h
+++ lv_menu.h
@@ -24,21 +24,19 @@ typedef struct {
     char ** menu_labels;
     lv_event_cb_t * list_of_callbacks;
 }MenuItem;
-//хранилище всех подменю, содержит в себе подменю и их координаты
+
+typedef struct {
+    int curent_level;
+    lv_obj_t * first_btn;
+    lv_obj_t ** visible_menu_list;
+}visible_menu;
+
 typedef struct {
     MenuItem ** MenuBase;
     int ** CoordBase;
     int DB_size;
-}Menu_DB;
-
-
-typedef struct {
-    Menu_DB * DataBase;
+    visible_menu * vis_menu;
     lv_group_t * maingroup;
-    int curent_level;
-    lv_obj_t * first_btn;
-    MenuItem * first_menu;
-    lv_obj_t ** visible_menu_list;
     lv_style_t * opa_style;
 }MENU;
 
@@ -50,16 +48,19 @@ typedef struct{
     MENU * menu;
 }BtnItem;
 
+typedef struct {
+    lv_btn_ext_t btn;
+    MENU * menu;
+}special_menu_btn;
+
 /**********************
  * GLOBAL PROTOTYPES
  **********************/
-MenuItem * get_menu_item(Menu_DB * DataBase,int * coord);
+MenuItem * get_menu_item(MENU * menu,int * coord);
 
 int * create_coord_prev_menu(int * coord);
 
-void init_menu_item(Menu_DB * DataBase,int * coord);
-
-int * create_coord_next_menu(int level,void * coord,int btn_number);
+int * create_coord_next_menu(int * coord,int btn_number);
 
 void standart_menu_cb(lv_obj_t * triger_btn,lv_event_t event);
 
@@ -67,23 +68,19 @@ void rewrite_group(lv_group_t * maingroup,lv_obj_t * previous_menu);
 
 void remove_menu(lv_group_t * maingroup,lv_obj_t * menu);
 
-void set_menu_labels(MENU * mainmenu,int * coord,char ** labels);
+void set_menu_labels(MENU * menu,int * coord,char ** labels);
 
-void set_menu_size(MENU*mainmenu,int * coord,int height,int weight);
+void set_menu_size(MENU * mainmenu,int * coord,int height,int weight);
 
 void set_menu_align(MENU * mainmenu,int * coord, lv_own_align_t align);
 
-void set_extr_attr(lv_obj_t * btn,int iter,MENU * menu,void * coord);
+void set_extr_attr(lv_obj_t * btn,int iter,MENU * menu,int * coord);
 
 void set_align_to_menu(lv_obj_t * triger_btn,lv_obj_t * list_obj,lv_own_align_t align);
 
-void make_and_show_menu(lv_obj_t * triger_btn,MENU * mainmenu,MenuItem * menu, void * coord);
+void make_and_show_menu(lv_obj_t * triger_btn,MENU * menu,int * coord);
 
-void lv_menu_create(MENU * menu,lv_obj_t * triger_btn,char ** labels);
-
-void init_menu(MENU * mainmenu,lv_obj_t * triger_btn);
-
-void menu_use(MENU * mainmenu);
+void lv_menu_create(lv_obj_t * triger_btn);
 
 void set_size_to_all_menu(MENU * mainmenu,int height,int weight);
 
@@ -94,6 +91,14 @@ char ** rewrite_labels(char ** labels);
 int * get_coord(MENU * mainmenu,int i);
 
 void set_menu_transparent(MENU * mainmenu);
+
+void set_cb_to_btn(MENU * mainmenu,int * coord,int btn_number,lv_event_cb_t callback);
+
+void close_all_menu(MENU * mainmenu);
+
+void using_menu(void);
+
+MENU * bind_menu_to_obj(lv_obj_t * triger_obj);
 
 int phys_btn_pressed(lv_key_t * key);
 #ifdef __cplusplus
